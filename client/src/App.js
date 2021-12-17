54ad7762553195d29ce05076c57d255d91e93b24
DeviousLab <deviouslab@gmail.com>
2021-12-18 01:15:32 +0400

    Added connection to backend with frontend values
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
   const [password, setPassword] = useState('');
   const [title, setTitle] = useState('');
 
+  const addPassword = async () => {
+    Axios.post('http://localhost:5000/addpasswords', {
+      password,
+      title
+    });
+  };
 
   return (
     <div className="App">
       <div className="add-password">
         <input type="text" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
         <input type="text" placeholder="Google" onChange={(e) => {setTitle(e.target.value)}}/>
-        <button>Add</button>
+        <button onClick={addPassword}>Add</button>
       </div>
     </div>
   );
