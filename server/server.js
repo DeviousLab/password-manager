b88234f69838ce4f60c1660dc34bb5067d098e01
DeviousLab <deviouslab@gmail.com>
2021-12-18 01:15:16 +0400

    Added connection to MySQL database
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const mysql = require('mysql');
+const cors = require('cors');
 
 const db = mysql.createConnection({
     user: process.env.DB_USER,
@@ -10,10 +11,23 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+app.use(cors());
+app.use(express.json());
+
 app.listen(8000, () => {
   console.log('Server started on port 8000');
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/addpasswords', (req, res) => {
+  const { password, title } = req.body;
+  db.query("INSERT INTO passwords (pw_passwords, pw_title) VALUES (?,?)", 
+  [password, title], 
+  (err, results) => {
+    if (err) throw err;
+    res.send(results);
+  });
 });
