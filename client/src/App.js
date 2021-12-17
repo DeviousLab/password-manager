f6ee52b73f512a6ea6b81797457804f3ad88326d
DeviousLab <deviouslab@gmail.com>
2021-12-18 00:53:56 +0400

    Setup basic frontend and react hooks state
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [title, setTitle] = useState('');
+
+
   return (
     <div className="App">
-      
+      <div className="add-password">
+        <input type="text" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
+        <input type="text" placeholder="Google" onChange={(e) => {setTitle(e.target.value)}}/>
+        <button>Add</button>
+      </div>
     </div>
   );
 }
