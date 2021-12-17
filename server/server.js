4f7e9343ff91c15bb9c77fc648947a92463bf33d
DeviousLab <deviouslab@gmail.com>
2021-12-18 00:31:00 +0400

    Established connection to mysql server
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+const mysql = require('mysql');
 
+const db = mysql.createConnection({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME
+});
 
 app.listen(8000, () => {
   console.log('Server started on port 8000');
