ac119d02eab2e58f6962bd15d17db03a62ff0a74
DeviousLab <deviouslab@gmail.com>
2021-12-18 03:44:51 +0400

    Added encrypt method just before db entry
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const mysql = require('mysql2');
 const cors = require('cors');
 
+const { encrypt, decrypt } = require('./encryption-handler');
+
 const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -24,10 +26,15 @@ app.get('/', (req, res) => {
 
 app.post('/addpasswords', (req, res) => {
   const { password, title } = req.body;
-  db.query("INSERT INTO passwords (pw_passwords, pw_title) VALUES (?,?)", 
-  [password, title], 
+  const encryptedPassword = encrypt(password);
+  db.query("INSERT INTO passwords (pw_passwords, pw_title, pw_iv) VALUES (?, ?, ?)", 
+  [encryptedPassword.password, title, encryptedPassword.iv],
   (err, results) => {
     if (err) throw err;
     res.send(results);
   });
+});
+
+app.post('/decryptpasswords', (req, res) => {
+  res.send(decrypt(req.body));
 });
