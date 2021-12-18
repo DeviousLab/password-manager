f6e051aa321abdf5d6019a9034f4731b50241918
DeviousLab <deviouslab@gmail.com>
2021-12-18 18:48:17 +0400

    Implemented method to display password added to db
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Axios from 'axios';
 
 function App() {
   const [password, setPassword] = useState('');
   const [title, setTitle] = useState('');
+  const [passwordsArray, setPasswordsArray] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:8000/getpasswords')
+      .then(res => {
+        setPasswordsArray(res.data);
+      })
+      .catch(err => console.log(err));
+  });
 
   const addPassword = () => {
     Axios.post('http://localhost:8000/addpasswords', {
@@ -13,12 +22,32 @@ function App() {
     });
   };
 
+  const showPassword = (encryption) => {
+    Axios.post('http://localhost:8000/decryptpasswords', {
+      password: encryption.pw_passwords,
+      iv: encryption.pw_iv
+    })
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
       <div className="add-password">
         <input type="text" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
         <input type="text" placeholder="Google" onChange={(e) => {setTitle(e.target.value)}}/>
         <button onClick={addPassword}>Add</button>
+      </div>
+      <div className="passwords-array">
+        {passwordsArray.map((index, key) => {
+          return (
+            <div className="password-element" onClick={() => {showPassword({ password: index.password, iv: index.iv })}} key={key}>
+              <p>{index.pw_title}</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   );
